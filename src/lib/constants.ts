03b3d5218852d41d0c6956e91ae92b4f39b0c1fd
Alejandro Sandí <alesf22@gmail.com>
2024-05-16 10:25:10 -0600

    chore: add constants for the app

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const CLIENT_APP_URL = process.env.CLIENT_APP_URL;
+export const PUBLIC_CLIENT_APP_URL = process.env.NEXT_PUBLIC_CLIENT_APP_URL;
+
+export const JWT_SECRET: string = process.env.JWT_SECRET;
