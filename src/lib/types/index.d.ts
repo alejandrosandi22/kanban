a8aef38e09f66866d1dd80365684caef818c3013
Alejandro Sandí <alesf22@gmail.com>
2024-05-17 21:29:10 -0600

    feat: set up supabase clients and middleware

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,5 +1,43 @@
 import { ReactNode } from 'react';
 
-interface LayoutProps {
+export type LayoutProps = {
   children: ReactNode;
+};
+
+export type SuccessState<T> = {
+  message: string;
+  data: T;
+};
+
+export type ErrorState = {
+  message: string;
+  type?: string;
+};
+
+export type FormErrorState = {
+  error: ErrorState | null;
+};
+
+export interface QueryResponse<T> extends FormErrorState {
+  success: SuccessState<T> | null;
+}
+
+export interface SuccessMutationResponse {
+  success: {
+    message: string;
+    data?: any;
+  } | null;
+}
+
+export interface MutationResponse
+  extends FormErrorState,
+    SuccessMutationResponse {}
+
+export interface ChartDataValues {
+  name: string;
+  value: string;
+}
+
+export interface StatsProps {
+  data: ChartDataValues[] | undefined;
 }
